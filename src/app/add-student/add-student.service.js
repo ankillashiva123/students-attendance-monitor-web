bc9aadb24b8908acfa96f0ae4a4e7f473cc946a6
ankillashiva123 <ankillashiva123@gmail.com>
2018-07-02 18:45:26 +0530

    Update add-student.service.js

--- src/app/add-student/add-student.service.js
+++ src/app/add-student/add-student.service.js
@@ -7,7 +7,7 @@
         .module('app')
         .service('addStudentService', function ($http) {
             this.addStudent = function (arg, callback) {
-                return $http.post('http://localhost:3000/studentsList/101', arg)
+                return $http.post('http://student-attendance-services.herokuapp.com/studentsList/101', arg)
                     .then(function (response) {
                         // login successful if there's a token in the response
                         if (response) {
